d4129c9a612fdf248675982159e8b0d9e6dbd6ba
abdallaamin <abdo.faozy@gmail.com>
2022-03-26 03:31:55 +0200

    initially done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,32 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+        <LinkTitle>Call</LinkTitle>
+        <LinkItem href="tel:+201145572259">+201145572259</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+        <LinkTitle>Email</LinkTitle>
+        <LinkItem href="mailto:abdo.faozy@gmail.com">abdo.faozy@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>chill ya man</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/abdallaamin" target="_blank" rel="github">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/abdalla-amin-2548ba122/" target="_blank" rel="linkedin">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://www.instagram.com/theaminoz/" target="_blank" rel="instagram">
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
